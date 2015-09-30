50b7b53443af6d50329871f7b308997a5de5fe68
Trung Hieu Nguyen <git@github.com:pdkhuong/VideoFW.git>
2015-09-30 23:27:02 +0700

    no message

--- application/views/video/_video_player.php
+++ application/views/video/_video_player.php
@@ -1,6 +1,8 @@
 <link href="<?php echo $theme_path ?>css/video-js.css" rel="stylesheet">
 <script src="<?php echo $theme_path ?>js/video-js.js"></script>
 <script src="<?php echo $theme_path ?>js/jquery.cookie.js"></script>
+<link href="<?php echo $theme_path ?>css/video-quality-selector.css" rel="stylesheet" type="text/css" />
+<script src="<?php echo $theme_path ?>js/video-quality-selector.js"></script>
 <script>
   videojs.options.flash.swf = "<?php echo $theme_path?>/swf/video-js.swf";
 </script>
@@ -15,7 +17,7 @@
             $streamingUrl = base64_decode($tmpStreamingUrlArr['streaming_url']);
           ?>
             <video id="_videojs" class="video-js vjs-default-skin" controls="" autoplay preload="" width="854" height="520" data-setup="{}">
-              <source src="<?php echo $streamingUrl?>" type="video/mp4">
+              <source src="<?php echo $streamingUrl?>" type="video/mp4" data-res="360">
             </video>
           <?php else:?>
             Video will update as soon as possible. Thank you!
