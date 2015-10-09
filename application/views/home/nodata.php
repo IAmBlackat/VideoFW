475cd493e99dffa278b968aa359555fb4a687831
KhuongPham <pham_dinhkhuong_tt@yahoo.com>
2015-10-09 12:21:08 +0700

    add nodata page

--- application/views/home/nodata.php
+++ application/views/home/nodata.php
@@ -1,1 +1,18 @@
-no data
+<div class="container">
+  <div class="row">
+    <?php $this->load->view('templates/sidebar'); ?>
+    <div class="wrap-content col-sm-9">
+      <div class="section-line">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="no-data">
+              <p>
+                Data not found or updating - Please come back again  - Thanks and enjoy!!!.
+              </p>
+            </div><!-- /.item-video -->
+          </div><!-- /.col-sm-6 -->
+        </div><!-- /.row -->
+      </div>
+    </div>
+  </div>
+</div>
